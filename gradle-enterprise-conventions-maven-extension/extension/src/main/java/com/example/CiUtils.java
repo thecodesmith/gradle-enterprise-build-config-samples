e8b384c59868e6e742c687d377fad0beef8066f2
Clay Johnson <cjohnson@gradle.com>
2023-03-27 10:35:33 -0500

    Add comment for examples of CI detection logic

--- gradle-enterprise-conventions-maven-extension/extension/src/main/java/com/example/CiUtils.java
+++ gradle-enterprise-conventions-maven-extension/extension/src/main/java/com/example/CiUtils.java
@@ -7,7 +7,9 @@ final class CiUtils {
     private CiUtils() {
     }
 
-    //todo: implement this more specifically for your CI environment
+    //todo: implement this more specifically for your CI environment.
+    // For provider-specific examples, see:
+    // https://github.com/gradle/common-custom-user-data-maven-extension/blob/main/src/main/java/com/gradle/CiUtils.java
     static boolean isCi() {
         return envVariable("CI").isPresent() || sysProperty("CI").isPresent();
     }
